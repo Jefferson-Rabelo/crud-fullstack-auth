1da1a41bcc7961b1df939f22974811810f54bb64
Jefferson Rabelo <jeffersonrabelo1221@gmail.com>
2026-05-22 18:59:52 -0300

    Configurando variáveis de ambiente

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
     //URL DO BACKEND DO RENDER
-    baseURL: 'https://crud-fullstack-auth.onrender.com'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 api.interceptors.request.use(
