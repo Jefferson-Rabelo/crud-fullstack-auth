ad021318c1f96ca0c106c40fe31aa6cb893ab295
Jefferson Rabelo <jeffersonrabelo1221@gmail.com>
2026-05-24 22:31:56 -0300

    feact: testando conecção com banco

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -2,23 +2,19 @@ const mysql = require('mysql2')
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-
-    ssl: {
-        rejectUnauthorized: false
-    }
+    port: process.env.DB_PORT
 })
 
 db.connect((err) => {
+
     if (err) {
         console.log('Erro ao conectar no banco', err)
-        return
+    } else {
+        console.log('MySQL conectado')
     }
-
-    console.log('MySQL conectado')
 })
 
 module.exports = db
