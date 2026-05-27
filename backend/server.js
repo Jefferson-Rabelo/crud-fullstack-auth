f29585e485fa7ec77a6bcc120bae193e7dc0814f
Jefferson Rabelo <jeffersonrabelo1221@gmail.com>
2026-05-27 19:45:13 -0300

    fix: configure cors for vercel

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,23 @@
 require('dotenv').config();// Carrega variáveis de ambiente do arquivo .env
 require('./src/database/db');
 const express = require('express');//IMPORTA O EXPRESS/CRIA O SERVIDOR
-const cors = require('cors');//Ele permite que um domínio diferente acesse sua API/CONEXAO BACK-END COM FRONT-END
-const userRoutes = require('./src/routes/userRoutes');
-const app = express();//CRIA O SERVIDOR
+const userRoutes = require('./src/routes/userRoutes')
+const cors = require('cors')//Ele permite que um domínio diferente acesse sua API/CONEXAO BACK-END COM FRONT-END
+const app = express()//CRIA O SERVIDOR
 
 app.use(cors({
     origin: [
-        'https://crud-fullstack-auth-git-main-jefferson-rabelos-projects.vercel.app',
-        'https://crud-fullstack-auth-2og9ssqgg-jefferson-rabelos-projects.vercel.app'
+        'http://localhost:5173',
+        'https://crud-fullstack-auth.vercel.app'
     ],
     credentials: true
-}));//permite acesso externo (Front-end React)
-app.use(express.json());//Permite que a API receba JSON no corpo das requisições
-app.use('/api', userRoutes);
+}))//permite acesso externo (Front-end React)
+app.use(express.json())//Permite que a API receba JSON no corpo das requisições
+app.use('/api', userRoutes)
 // Rota de teste para verificar se a API está funcionando
 app.get('/', (req, res) => {
-    res.send('API rodando 2026 🚀');
-});
+    res.send('API rodando 2026 🚀')
+})
 
 //Porta dinâmica para suportar ambiente de produção (Render, Railway, etc)
 const PORT = process.env.PORT || 3000;
